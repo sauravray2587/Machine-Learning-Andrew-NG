fc976af1b4fe090929ca9ed39e40dd52725afa64
sauravray2587 <sauravray2587@gmail.com>
2017-12-06 11:30:46 +0530

    Update computeCost.m

--- computeCost.m
+++ computeCost.m
@@ -12,14 +12,14 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
+temp=X*theta;
+sq_errors=(temp-y) .^2;
+J=sum(sq_errors)/(2*m);
 
 
 
 
 % =========================================================================
 
-temp=X*theta;
-sq_errors=(temp-y) .^2;
-J=sum(sq_errors)/(2*m);
+
 end
